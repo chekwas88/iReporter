efa6860f9d11f3ca74a7f4124c878d5af031110b
chekwas88 <princechekwas@gmail.com>
2018-11-25 22:25:21 +0100

    create a record

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require('express');
-const response = require('./model/response');
+const Incident = require('./model/incident');
 
 const app = express();
+app.use(express.json());
 
 app.get('/api/v1/red-flags', (req, res) => {
   const status = res.statusCode;
-  response.status = status;
-  res.send(response);
+  Incident.status = status;
+  res.send(Incident);
+});
+
+app.post('/api/v1/red-flags', (req, res) => {
+  const status = res.statusCode;
+  Incident.status = status;
+  const redFlag = {
+    id: Incident.incidents.length,
+    createdBy: req.body.createdBy,
+    createdOn: req.body.createdOn,
+    type: req.body.type,
+    title: req.body.title,
+    comment: req.body.comment,
+    location: req.body.location,
+  };
+  Incident.incidents.push(redFlag);
+  res.send(Incident);
 });
 
 const port = process.env.PORT || 4001;
---
+++ model/user.js
@@ -0,0 +1,18 @@
+const User = {
+  status: null,
+  users: [
+    {
+      id: 0,
+      firstname: '',
+      lastname: '',
+      username: '',
+      email: '',
+      admin: '',
+
+    },
+
+  ],
+
+};
+
+module.exports = User;
